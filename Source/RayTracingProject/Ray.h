c62acb2e47ec5cddb31daf181fecf05419b54886
StephenieDugan <stepheniedugan617@gmail.com>
2023-11-28 11:25:57 -0700

    Did the Ray exercise.

---
+++ Source/RayTracingProject/Ray.h
@@ -0,0 +1,17 @@
+#include <glm\glm.hpp>
+
+
+struct ray_t
+{
+	ray_t() = default;
+	ray_t(const glm::vec3& origin, const glm::vec3& direction) :
+		m_origin{origin},
+		m_direction{direction}
+	{}
+
+	glm::vec3 GetPoint(float distance) const { return m_origin * (m_direction * distance); }
+	glm::vec3 operator *(float distance) { return m_origin * (m_direction * distance); }
+
+	glm::vec3 m_origin;
+	glm::vec3 m_direction;
+};
